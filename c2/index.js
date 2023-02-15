9a881cc19e628e7e6f937da69d3d8debd7d1b6c2
Zhivko Apostoloski <zhivko.apostoloski@gmail.com>
2023-02-15 22:02:14 +0100

    commit c2 final

--- c2/index.js
+++ c2/index.js
@@ -52,3 +52,84 @@ const testFn1 = () => {
 }
 
 testFn1();
+
+// const testFn2 = (b, a = 25) => {
+//     return a + b;
+// }
+
+// console.log(testFn2(50, 20));
+
+const testFn3 = (o = {}) => {
+    o.ime = "Zhivko";
+    console.log(o);
+}
+
+const s = { ime: "Zlate", prezime: "Zlatevski" };
+testFn3(s);
+// console.log(o);
+console.log(s);
+
+let uchenik = { ime: "Marko", ocena: 4 };
+let uchenik2 = uchenik;
+
+console.log(uchenik);
+uchenik.ime = "Vlado";
+console.log(uchenik);
+console.log(uchenik2);
+
+// const convert = (type, value) => {
+//     let returnValue = null;
+//     switch (type) {
+//         case 'c2f':
+//             returnValue = value * 9/5 + 32;
+//             break;
+//         case 'f2c':
+//             returnValue = (value - 32) * 5/9;
+//             break;
+//         default:
+//             console.log('Cannot convert. The conversion type does not exist.');
+//             break;
+//     }
+
+//     return returnValue;
+// }
+
+const convert = (type, value) => {
+    switch (type) {
+        case 'c2f':
+            return value * 9/5 + 32;
+        case 'f2c':
+            return (value - 32) * 5/9;
+        default:
+            console.log('Cannot convert. The conversion type does not exist.');
+            return;
+    }
+}
+
+let temp = 25;
+console.log(convert('c2f', temp));
+let res = convert('p2p', temp);
+console.log(res);
+// c2f vrednost * 9/5 + 32 
+// f2c (vrednost - 32) * 5/9
+
+// definirajte dva objekti na uchenici
+// da imaat ime, prezime, ocenka
+// dokolku prviot uchenik ima pogolema ocena od vtoriot
+// pechatete "<ime na prv uchenik> <prezime na prv uchenik> e podobar od <ime na vtor uchenik> <prezime na vtor uchenik>"
+// dokolku e obratno, ispechatete go obratnoto
+// dokolku se isti, ispechatete, "<uchenik 1> ima isto znaenje kako i <uchenik 2>"
+
+const prvUchenik = { ime: "Stefan", prezime: "Petreski", ocenka: 5 };
+const vtorUchenik = { ime: "Filip", prezime: "Stefanoski", ocenka: 4 };
+if (prvUchenik.ocenka > vtorUchenik.ocenka) {
+    console.log(`${prvUchenik.ime} ${prvUchenik.prezime} e podobar od ${vtorUchenik.ime} ${vtorUchenik.prezime}`);
+} else if (prvUchenik.ocenka < vtorUchenik.ocenka) {
+    console.log(`${vtorUchenik.ime} ${vtorUchenik.prezime} e podobar od ${prvUchenik.ime} ${prvUchenik.prezime}`);
+} else {
+    console.log(`${vtorUchenik.ime} ${vtorUchenik.prezime} ima isto znaenje kako i ${prvUchenik.ime} ${prvUchenik.prezime}`);
+}
+
+prvUchenik.ocenka > vtorUchenik.ocenka ?
+    console.log('asd') : prvUchenik.ocenka < vtorUchenik.ocenka ?
+        console.log('asd1') : console.log('asd2');
