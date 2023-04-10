5edabd4f370d32cb4ad3f44f7a153e13ba7c36c7
Zhivko Apostoloski <zhivko.apostoloski@gmail.com>
2023-04-10 22:02:46 +0200

    ws3 full (environment variables)

--- ws2/index.js
+++ ws2/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const auth = require('./handlers/auth');
 const { expressjwt: jwt } = require('express-jwt');
 const db = require('./pkg/db');
+const userModel = require('./pkg/user');
+const config = require('./pkg/config');
 
 db.init();
 
@@ -11,7 +14,8 @@ api.use(express.json());
 // use jwt
 api.use(jwt({
     algorithms: ['HS256'],
-    secret: 'zhivko123'
+    // secret: process.env.JWT_SECRET
+    secret: config.get('settings').jwt_secret
 }).unless({
     path: [
         '/api/v1/auth/sign-up',
@@ -25,26 +29,28 @@ api.post('/api/v1/auth/sign-up', auth.signUp);
 api.post('/api/v1/auth/login', auth.login);
 // api.post('/api/v1/auth/forgot-password', auth.forgotPassword);
 // api.post('/api/v1/auth/reset-password', auth.resetPassword);
-api.get('/api/v1/profile', (req, res) => {
+api.get('/api/v1/profile', async (req, res) => {
     try {
         // nekoja biznis logika
-        res.status(200).send('OK');
+        let user = null;
+        user = await userModel.getUserByEmail(req.body.email);
+        res.status(200).send(user);
     } catch (err) {
         console.log(err);
         return res.status(500).send('Internal Server error');
     }
 })
 
-api.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send('Invalid token');
-    } else {
-        next()
-    }
-});
+// api.use(function (err, req, res, next) {
+//     if (err.name === 'UnauthorizedError') {
+//         res.status(401).send('Invalid token');
+//     } else {
+//         next()
+//     }
+// });
 
-api.listen(10000, err => {
+api.listen(config.get('settings').server_port, err => {
     if (err) return console.log(err);
 
-    console.log('Server successfully started...');
+    console.log(`Server successfully started at port: ${config.get('settings').server_port}...`);
 });
--- ws2/pkg/db/index.js
+++ ws2/pkg/db/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const config = require('../config');
 
 const init = () => {
-    const dsn = 'mongodb+srv://zhivko:xx5pAIg0QFcszlrS@cluster0.mcazydz.mongodb.net/Movies?retryWrites=true&w=majority'
+    // const dsn = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    const url = config.get('settings').db_url;
+    const dbname = config.get('settings').db_name;
+    const username = config.get('settings').db_username;
+    const password = config.get('settings').db_password;
+    const dsn = `mongodb+srv://${username}:${password}@${url}.net/${dbname}?retryWrites=true&w=majority`;
     mongoose.connect(dsn);
 };
 
